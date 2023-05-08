83a6c11045d626636b98ac67f46ddf15fc41a8e5
Al2003x <Danilovvik@bk.ru>
2023-05-08 19:13:07 +0300

    feat: did task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const path = require('path');
+const fs = require('fs/promises');
+const dir = path.join(__dirname, 'files');
+const dircopy = path.join(__dirname, 'files-copy');
 
+fs.rm(dircopy, {
+  recursive: true,
+  force: true
+}).finally(function() {
+  fs.mkdir(dircopy, {
+      recursive: true
+  });
+  fs.readdir(dir, {
+      withFileTypes: true
+  }).then(function(data) {
+      data.forEach(function(item) {
+          if (item.isFile()) {
+              let pathItem = path.join(dir, item.name);
+              let pathItemDes = path.join(dircopy, item.name);
+              fs.copyFile(pathItem, pathItemDes);}
+      });
+  });
+});
